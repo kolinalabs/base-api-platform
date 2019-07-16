f044fef7cd1d03b0a7ec0ab0c356802bb907cee5
Jonadabe S. Nascimento <jhonndabi.s.n@hotmail.com>
2019-07-16 16:59:57 -0300

    Added doctrine extensions bundle as dependency of the project

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
