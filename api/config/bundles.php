66fe1e1f88fd0835a4332b245faa79bb3642defe
Jonadabe S. Nascimento <jhonndabi.s.n@hotmail.com>
2019-07-16 17:52:49 -0300

    Added web profiler as dependency of the project

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
