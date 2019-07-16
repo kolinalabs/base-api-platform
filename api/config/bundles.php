1ea581ee4648acdab14cb2357fc7e222189f64ab
Jonadabe S. Nascimento <jhonndabi.s.n@hotmail.com>
2019-07-16 18:14:23 -0300

    Added nelmio alice bundle as dependency of the project

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -13,4 +13,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
+    Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
 ];
